bbb3b2ce07f543f9109c5c1afccd488436b36933
{nooramoi} <{t1mono00@students.oamk.fi}>
2023-09-05 19:53:21 +0300

    Viikkotehtävä 1

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Text } from 'react-native';
+import { Card } from 'react-native-paper';
 
-export default function App() {
+const HeartRateCalculator = () => {
+  const [age, setAge] = useState('');
+  const [lowerLimit, setLowerLimit] = useState('');
+  const [upperLimit, setUpperLimit] = useState('');
+
+  useEffect(() => {
+    if (age !== '') {
+      const ageNumber = parseInt(age);
+      if (!isNaN(ageNumber)) {
+        const lower = Math.round((220 - ageNumber) * 0.65);
+        const upper = Math.round((220 - ageNumber) * 0.85);
+        setLowerLimit(lower.toString());
+        setUpperLimit(upper.toString());
+      }
+    } else {
+      setLowerLimit('');
+      setUpperLimit('');
+    }
+  }, [age]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Card>
+        <Card.Content>
+          <Text style={{ fontSize: 20 }}>Heart Rate Calculator</Text>
+          <TextInput
+            placeholder="Enter Age"
+            keyboardType="numeric"
+            value={age}
+            onChangeText={(text) => setAge(text)}
+            style={{ fontSize: 18, marginTop: 20 }}
+          />
+          <Text style={{ fontSize: 18, marginTop: 20 }}>
+            Lower Limit: {lowerLimit}
+          </Text>
+          <Text style={{ fontSize: 18, marginTop: 10 }}>
+            Upper Limit: {upperLimit}
+          </Text>
+        </Card.Content>
+      </Card>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default HeartRateCalculator;
